3f456202e9cc7b275a69223442f800e4f8568f13
khayrat18 <hairatua@yahoo.com>
2023-06-08 07:09:11 -0700

    py import-modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 #import a  module
-import calculator_1
+import calculator_1 
+
 #declare variables
 a = int(10)
 b = int(5)
 
 #printing result of the function
 
-print("{} + {} = {}".format(a, b, a + b))
-print("{} - {} = {}".format(a, b, a - b))
-print("{} * {} = {}".format(a, b, a * b))
-print("{} / {} = {}".format(a, b, a // b))
+print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
 
