e04aa5ee357656a7c850830b4f1ed938fe59832d
khayrat18 <hairatua@yahoo.com>
2023-06-11 02:54:01 -0700

    py data-structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,6 @@
 
 def element_at(my_list, idx):
 
-    for idx in my_list:
-        if (idx < 0) and (idx > my_list):
-            return None
-    return(idx - 1)
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@
 def print_matrix_integer(matrix=[[]]):
 
     for i in matrix:
-        print("{}".format(i))
+        print("".join("{:2d}".format(i)))
