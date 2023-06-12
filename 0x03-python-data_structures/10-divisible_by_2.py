685b589d87566114251ab4e5c3b4fa90eb38cdb5
khayrat18 <hairatua@yahoo.com>
2023-06-12 04:26:21 -0700

    py data-structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-   
+
 def divisible_by_2(my_list=[]):
     result = []
     if my_list is None:
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
         my_list = []
     for num in my_list:
         if num > largest:
-             largest = num
+            largest = num
     return largest
