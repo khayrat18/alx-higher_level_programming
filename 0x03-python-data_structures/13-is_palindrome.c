b3e0c3855dc955322660f72b5ed4f4033bd42f11
khayrat18 <hairatua@yahoo.com>
2023-06-13 15:07:59 -0700

    py data-structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,64 +8,64 @@
 
 listint_t *reversed_listint(listint_t **head)
 {
-    listint_t *node = *head, *next, *prev = NULL;
-    while (node)
-    {
-        next = node->next;
-        node->next = prev;
-        prev = node;
-        node = next;
-    }
-    *head = prev;
-    return (*head);
+	listint_t *node = *head, *next, *prev = NULL;
 
-
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+	*head = prev;
+	return (*head);
 }
 
 /**
- * is_palindeome - a function that checks if a given list is a palindrome
+ * is_palindrome - a function that checks if a given list is a palindrome
  * @head: adress of the head node
  * Return: 0 if the list is not a palindrome, otherwise 1
-*/
+ */
+
 int is_palindrome(listint_t **head)
 {
-    listint_t *holder, *rev, *mid;
-    size_t size = 0; i;
-    
-    if (*head == NULL || (*head)->next == NULL)
-    {
-        return (1);
-    }
+	listint_t *holder, *rev, *mid;
+	size_t size = 0;
+	size_t i;
 
-    *head= *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (1);
+	}
 
-    while(holder)
-    {
-        size++;
-        holder = holder->next;
-    }
+	holder = *head;
 
-    holder = *head;
-    for (i = 0; i < (size/2)-1; i++)
-        holder = holder-> next;
-    if ((size % 2) == 0 && holder->n != holder->next->n)
-        return (0);
+	while (holder)
+	{
+		size++;
+		holder = holder->next;
+	}
 
-    holder = holder->next->next;
-    rev = reversed_listint(&holder);
-    mid =  rev;
+	holder = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		holder = holder->next;
+	if ((size % 2) == 0 && holder->n != holder->next->n)
+		return (0);
 
-    holder = *head;
+	holder = holder->next->next;
+	rev = reversed_listint(&holder);
+	mid =  rev;
 
-    while(rev)
-    {
-        if(holder->n != rev->n)
-            return(0);
-        holder = holder->next;
-        rev = rev->next;
-        
-    }
-    reversed_listint(&mid);
-    return (i);
+	holder = *head;
+
+	while (rev)
+	{
+	if (holder->n != rev->n)
+		return (0);
+	holder = holder->next;
+	rev = rev->next;
+	}
+	reversed_listint(&mid);
+	return (i);
 
 }
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    
-           for row in range (len(matrix)):
-            for col in range (len(matrix[row])):
-                print("{:d}".format(matrix[row][col]), end="")
-                if col != (len(matrix[-1])-1):
-                     print (" ", end="")
-            print("")
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][col]), end="")
+            if col != (len(matrix[-1])-1):
+                print(" ", end="")
+        print("")
