4bb993f98a2851689e3874014c41e97dc071342f
Hairatu Lansah Adam <hairatua@yahoo.com>
2023-06-26 14:03:46 +0000

    py-exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+# #!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for idx in my_list[:x]:
+            if isinstance(idx, int):
+                print("{:d}".format(idx), end="")
+                count += 1
+        print()
+    except TypeError:
+        pass
+        return 0
+    else:
+        return (count)
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+# #!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            x = my_list_1[i]
+            y = my_list_2[i]
+            quotient = x / y
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        except TypeError:
+         print("wrong type")
+         quotient = 0
+        except IndexError:
+            print("out of range")
+            quotient = 0
+        finally:
+            result.append(quotient)
+        return (result)
