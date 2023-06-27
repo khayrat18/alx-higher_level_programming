f9b513a2e4c7fbd2116026753959c7a55ab00c1d
khayrat18 <hairatua@yahoo.com>
2023-06-27 15:20:26 -0700

    py-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
-class square:
-    """ A class that defines an empty square object"""
-
-    def __init__(self) -> None:
-        pass
-
-    def __init__(self) -> None: 
-        pass
-    """Initializes a square object
-    Args: None
-    Returns: None
-    Raises: None
+class Square():
     """
+    A class that defines an empty square object
+    """
+
+    def __init__(self):
+
+        """
+        Initializes a square object
+        Args:
+            None
+        Returns:
+            None
+        Raises:
+            None
+        """
+        pass
