5b91f73910f6cc98c49cbb0dac7af62695f5c2fe
khayrat18 <hairatua@yahoo.com>
2023-06-27 15:35:11 -0700

    py-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,23 +2,15 @@
 
 """An class that defines a square"""
 
+
 class Square:
-<<<<<<< HEAD
 
-    """Initialiaze a square object with a given size
-    Args: size
-    Returns: None"""
-
-    def __init__(self,size):
-        self._size = size
-=======
-    """A class that represents a square object
-    """
-    def __init__(self,size):
-        """ Initialiazes a square object
-        Args: Size
-        Returns: None
-        Raises: None
+    def __init__(self,  size):
+        """
+        Initialiaze a square object with a given size
+        Args:
+            size(int)
+        Returns:
+            None
         """
         self.__size = size
->>>>>>> 81d877101146fcd141ac956aefe4330e92e8f253
