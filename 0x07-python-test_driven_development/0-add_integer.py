a9b8e88ddf66d48c76a813afe1855467b95d4971
Hairatu Lansah Adam <hairatua@yahoo.com>
2023-06-29 16:04:54 +0000

    py-tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,12 @@
 # #!/usr/bin/python3
 
 def add_integer(a, b=98):
-
-    if not isinstance(a,int) or not isinstance(b,int):
-        raise ValueError("")
     
-    return 
+    if type(a) not in[int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int,float]:
+        raise TypeError("b must be an integer")
+    
+    sum = int(a) + int(b)
+    
+    return sum
