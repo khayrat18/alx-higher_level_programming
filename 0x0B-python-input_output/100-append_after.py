8f7cac1b6896c373d1365e5dfe6355c0745e7871
khayrat18 <hairatua@yahoo.com>
2023-07-11 17:29:54 -0700

    py-I/O

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,4 +14,4 @@ def append_after(filename="", search_string="", new_string=""):
             if search_string in line:
                 text += new_string
     with open(filename, 'w') as wf:
-        w.write(text)
+        wf.write(text)
